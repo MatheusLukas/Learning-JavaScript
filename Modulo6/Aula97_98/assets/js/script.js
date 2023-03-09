f4bf3f3532c1f57802a7c893deee048ee1857e2f
MatheusLukas <matheuslukas636@gmail.com>
2023-03-09 11:19:03 -0300

    Exercise completed

--- Modulo6/Aula97_98/assets/js/script.js
+++ Modulo6/Aula97_98/assets/js/script.js
@@ -1,95 +1,120 @@
 const formulario = document.querySelector("#form");
-const result = document.querySelector("#result");
-let cpf;
-let cpfClean;
-let resultCpfSlice;
-let firstNumberCpf = 0;
-let secondNumberCpf = 0;
+const resultArea = document.querySelector("#result");
 
 // Função geral
 function sendForm(e) {
-  let count = 11;
-  let sumResult = 0;
   e.preventDefault();
-  function getCpf() {
-    cpf = document.querySelector("#cpf").value;
-    console.log(cpf);
+
+  const cpf = document.querySelector("#cpf").value;
+  let cpfClean;
+  let cpfArray = [];
+
+  function cpfForArray() {
     cpfClean = cpf.replace(/\D+/g, "");
-    resultCpfSlice = cpfClean.slice(0, -2);
-  }
-  // Funçaõ de checar o Numero
-  function checkFirstNumberCpf() {
-    // For que faz a conta dos digitos do cpf
-    for (i of resultCpfSlice) {
-      count--;
-      console.log(`i: ${i}`);
-      console.log(`count: ${count}`);
-      //   console.log(`cpf: ${cpfClean}`);
-      let resultCpfValue = i * count;
-      sumResult += resultCpfValue;
-      console.log(`result: ${sumResult}`);
-    }
-    // Verificar se o numero é maior do que 9
-    if ((firstNumberCpf = 11 - (sumResult % 11) > 9)) {
-      firstNumberCpf = 0;
-    } else firstNumberCpf = 11 - (sumResult % 11);
-    // Comparação do primeiro digito do cpf
-    if (firstNumberCpf === Number(cpfClean.slice(-2, -1))) {
-      console.log(
-        `cheguei do jeito certo: numero que eu quero: ${cpfClean.slice(
-          -2,
-          -1
-        )}, oque veio: ${firstNumberCpf}`
-      );
-      console.log("Estou na segunda etapa");
-      sumResult = 0;
-      count = 12;
-      resultCpfSlice = resultCpfSlice.slice(-1);
-      // for que faz a conta dos digitos do cpf
-      for (i of resultCpfSlice) {
-        count--;
-        console.log(`i: ${i}`);
-        console.log(`count: ${count}`);
-        let resultCpfValue = i * count;
-        sumResult += resultCpfValue;
-        console.log(`result: ${sumResult}`);
-      }
-      // Verifica se é maior que 9
-      if ((secondNumberCpf = 11 - (sumResult % 11) > 9)) {
-        secondNumberCpf = 0;
-        console.log("oi");
-      }
-      // Else
-      else {
-        result.innerHTML = "Seu CPF é inválido";
 
-        console.log(typeof secondNumberCpf, typeof Number(cpfClean.slice(-1)));
-        console.log(
-          `cheguei do jeito errado2: numero que eu quero: ${cpfClean.slice(
-            -1
-          )}, oque veio: ${secondNumberCpf}`
-        );
-      }
-
-      if (secondNumberCpf === Number(cpf.clean.slice(-1))) {
-        console.log("deu bom mano");
-      }
-    } // else do primeiro digito
-    else {
-      result.innerHTML = "Seu CPF é inválido";
-
-      //
-      //   console.log(
-      //     `cheguei do jeito errado: numero que eu quero: ${firstNumberCpf}, oque veio: ${cpfClean.slice(
-      //       -2,
-      //       -1
-      //     )}`
-      //   );
-    }
+    cpfArray = Array.from(cpfClean);
+    // console.log(separateCpf);
+    // console.log(cpfArray);
   }
 
-  getCpf();
-  checkFirstNumberCpf();
+  function numbersCpfCalculator() {
+    let separateCpf = cpfArray.slice(0, 9);
+    let firstNumberCpf;
+    let count = 10;
+    let result = 0;
+    let sumCpf = 0;
+    // console.log(`como chega: ${separateCpf}`);
+    for (i of separateCpf) {
+      sumCpf = i * count;
+      result += sumCpf;
+      --count;
+      // console.log(result);
+    }
+
+    comparateCpf = 11 - (result % 11);
+    // console.log(`first Number: ${comparateCpf}`);
+    // Comparações
+    // console.log(
+    //   typeof firstNumberCpf,
+    //   typeof Number(cpfArray[cpfArray.length - 2])
+    // );
+
+    if (comparateCpf > 9) {
+      comparateCpf = 0;
+
+      if (comparateCpf === Number(cpfArray[cpfArray.length - 2])) {
+        console.log("cheguei");
+        count = 11;
+        result = 0;
+        separateCpf = cpfArray.slice(0, 10);
+        for (i of separateCpf) {
+          sumCpf = i * count;
+          result += sumCpf;
+          --count;
+          console.log(result);
+        }
+        if (comparateCpf > 9) {
+          comparateCpf = 11 - (result % 11);
+          console.log("cheguei no if");
+          console.log(
+            `ComparateCpf = ${comparateCpf}, e o cpfArray = ${
+              cpfArray[cpfArray.length - 1]
+            }`
+          );
+          if (comparateCpf === Number(cpfArray[cpfArray.length - 1])) {
+            console.log("Cheguei aqui2");
+
+            resultArea.innerHTML = "Seu CPF é válido";
+          }
+        } else {
+          comparateCpf = 11 - (result % 11);
+          console.log("cheguei no if");
+          console.log(
+            `ComparateCpf = ${comparateCpf}, e o cpfArray = ${
+              cpfArray[cpfArray.length - 1]
+            }`
+          );
+          if (comparateCpf === Number(cpfArray[cpfArray.length - 1])) {
+            console.log("Cheguei aqui2");
+
+            resultArea.innerHTML = "Seu CPF é válido";
+          }
+        }
+      } else resultArea.innerHTML = "Seu CPF é inválido";
+      // console.log(cpfArray[cpfArray.length - 2]);
+    } else {
+      if (comparateCpf === Number(cpfArray[cpfArray.length - 2])) {
+        console.log("cheguei");
+        count = 11;
+        result = 0;
+        separateCpf = cpfArray.slice(0, 10);
+        for (i of separateCpf) {
+          sumCpf = i * count;
+          result += sumCpf;
+          --count;
+          console.log(result);
+        }
+        if (comparateCpf > 9) {
+        } else {
+          comparateCpf = 11 - (result % 11);
+          console.log("cheguei no if");
+          console.log(
+            `ComparateCpf = ${comparateCpf}, e o cpfArray = ${
+              cpfArray[cpfArray.length - 1]
+            }`
+          );
+          if (comparateCpf === Number(cpfArray[cpfArray.length - 1])) {
+            console.log("Cheguei aqui2");
+
+            resultArea.innerHTML = "Seu CPF é válido";
+          }
+        }
+      } else resultArea.innerHTML = "Seu CPF é inválido";
+    }
+  }
+
+  cpfForArray();
+  numbersCpfCalculator();
 }
 
 formulario.addEventListener("submit", sendForm);
